42a32852561418932e6a8f04678e99368dfdae4d
Grigore Budac <budacgrigore2000@gmail.com>
2019-11-15 21:30:25 +0100

    Christmas tree on the center

---
+++ src/screens/Home/Home.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const TreeWrapper = styled.View`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  margin: 20px 0px;
+`;
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { Image } from "react-native";
 
 import Container from "src/components/Container";
 import OptionToggle from "src/components/OptionToggle";
 import Divider from "src/components/Divider";
 
 import AppService from "src/services/AppService";
+import { TreeWrapper } from "./Home.styles";
 
 const Home = () => {
   const [lightsOn, setLightsOn] = useState(false);
@@ -16,6 +18,10 @@ const Home = () => {
 
   return (
     <Container scrollable>
+      <TreeWrapper>
+        <Image source={require("src/assets/tree.png")} />
+      </TreeWrapper>
+
       <OptionToggle
         title="Lights"
         description="Turn the tree's lights"
